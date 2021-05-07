91d181e9edb8e0f0bba83d27ddc01796c0c658b3
Az Vasquez <azvasquez99@gmail.com>
2021-05-06 20:05:13 -0400

    Remove explicit dora label

--- lib/widgets/dora.dart
+++ lib/widgets/dora.dart
@@ -12,17 +12,20 @@ class Dora extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topRight,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: new List.from(
-          [Padding(
-              child: Text('Dora'),
-              padding: EdgeInsets.all(4.0),
-          )]
-        )..addAll(
-          this.dora.map<Widget>((MTile mTile) => Tile(mTile, () {}, height: 32.0, width: 24.0)).toList()
+      child: Container(
+        margin: const EdgeInsets.all(4.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: new List.from(
+            [Padding(
+                child: Text('Dora'),
+                padding: EdgeInsets.all(4.0),
+            )]
+          )..addAll(
+            this.dora.map<Widget>((MTile mTile) => Tile(mTile, () {}, height: 32.0, width: 24.0)).toList()
+          ),
         ),
-      ),
+      )
     );
   }
 }
